fad8b55ee2e5e45615933f8143a877ecc32a2f69
Kim-Jin-Uk <gottkfthf@gmail.com>
2022-04-07 16:56:52 +0900

    Create  - Signup Page: all ui Update  - Home style sheet: extension  - Login Page: Link => div (for router replacement)

--- front/pages/signup/index.tsx
+++ front/pages/signup/index.tsx
@@ -4,19 +4,19 @@ import { MouseEventHandler, useCallback } from 'react'
 import SignWrapper from '../../componenets/SignWrapper'
 import styles from '../../styles/Sign.module.scss'
 
-const Login = () => {
-  const onClickSignUp: MouseEventHandler<HTMLDivElement> = useCallback(() => {
-    Router.replace('/signup')
+const Signup = () => {
+  const onClickLogIn: MouseEventHandler<HTMLDivElement> = useCallback(() => {
+    Router.replace('/login')
   }, [])
 
   return (
     <>
       <SignWrapper>
-        <div className={styles.title}>로그인</div>
+        <div className={styles.title}>회원가입</div>
         <div className={styles.sub_title}>
-          신규 사용자이신가요?&nbsp;
-          <div className={styles.signin} onClick={onClickSignUp}>
-            <span>계정 만들기</span>
+          기존 사용자이신가요?&nbsp;
+          <div className={styles.signin} onClick={onClickLogIn}>
+            <span>로그인 하기</span>
           </div>
         </div>
 
@@ -24,13 +24,13 @@ const Login = () => {
           <div className={`${styles.sns_btn} ${styles.naver_btn}`}>
             <div>
               <div className={styles.naver_icon}></div>
-              <div>네이버 아이디로 로그인</div>
+              <div>네이버 아이디로 회원가입</div>
             </div>
           </div>
           <div className={`${styles.sns_btn} ${styles.kakao_btn}`}>
             <div>
               <div className={styles.kakao_icon}></div>
-              <div>카카오 계정으로 로그인</div>
+              <div>카카오 계정으로 회원가입</div>
             </div>
           </div>
         </div>
@@ -39,4 +39,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Signup
